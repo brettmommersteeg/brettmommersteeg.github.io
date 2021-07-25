27a9858ac0c63c8632f667630f172c16f791c5b4
Alex Mommersteeg <alexmommersteeg@gmail.com>
2021-07-25 16:40:59 -0400

    transition on open and close

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
     $( "#nav-btn" ).click(function() {
         if($("#mobile-nav-id").height() > 0){
-          $("#mobile-nav-id").css('height','0');
           $("#mobile-nav-id").css('max-height','0');
-        }else{
-          $("#mobile-nav-id").css('height','auto');
+        }else{s
           $("#mobile-nav-id").css('max-height','600px');
         }
   });
