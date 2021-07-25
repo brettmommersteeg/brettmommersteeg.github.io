3a0a4f4942fbc490746237beafc44aff38c07b36
Alex Mommersteeg <alexmommersteeg@gmail.com>
2021-07-25 16:43:41 -0400

    typo

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
     $( "#nav-btn" ).click(function() {
         if($("#mobile-nav-id").height() > 0){
           $("#mobile-nav-id").css('max-height','0');
-        }else{s
+        }else{
           $("#mobile-nav-id").css('max-height','600px');
         }
   });
+  
 });
