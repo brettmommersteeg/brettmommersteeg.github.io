f0ecbaa862e66f345ba0dca686791956d93c6f39
Alex Mommersteeg <alexmommersteeg@gmail.com>
2021-07-25 16:38:22 -0400

    transition on mobile nav

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     $( "#nav-btn" ).click(function() {
         if($("#mobile-nav-id").height() > 0){
           $("#mobile-nav-id").css('height','0');
+          $("#mobile-nav-id").css('max-height','0');
         }else{
           $("#mobile-nav-id").css('height','auto');
+          $("#mobile-nav-id").css('max-height','600px');
         }
   });
 });
