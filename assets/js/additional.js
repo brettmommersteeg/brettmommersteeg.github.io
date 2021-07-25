4382c31061867ca1ad612bb5c19be5e03c37ce76
Alex Mommersteeg <alexmommersteeg@gmail.com>
2021-07-25 16:29:29 -0400

    update on click id

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $( "#mobile-nav" ).click(function() {
+    $( "#nav-btn" ).click(function() {
         if($(mobile-nav-id).height() > 0){
-        $(mobile-nav-id).css('height','0');
+          $(mobile-nav-id).css('height','0');
         }else{
-        $(mobile-nav-id).css('height','auto');
+          $(mobile-nav-id).css('height','auto');
         }
   });
 });
