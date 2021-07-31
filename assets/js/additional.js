42dd8ce4b90a56a201f5fab7808e48ee899c4120
Alex Mommersteeg <alexmommersteeg@gmail.com>
2021-07-31 15:17:57 -0400

    Image using js

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -16,19 +16,26 @@ $(document).ready(function(){
   var $images2 = $('archive img')
   console.log($images)
 
-  $images.viewer({
-    inline: true,
-    viewed: function() {
-      $image.viewer('zoomTo', 1);
-    } 
-  })
+  var viewer = new Viewer($images, {
+    url: 'data-original',
+    title: function (image) {
+      return image.alt + ' (' + (this.index + 1) + '/' + this.length + ')';
+    },
+  });
 
-  $images2.viewer({
-    inline: true,
-    viewed: function() {
-      $image.viewer('zoomTo', 1);
-    } 
-  })
+  // $images.viewer({
+  //   inline: true,
+  //   viewed: function() {
+  //     $image.viewer('zoomTo', 1);
+  //   } 
+  // })
+
+  // $images2.viewer({
+  //   inline: true,
+  //   viewed: function() {
+  //     $image.viewer('zoomTo', 1);
+  //   } 
+  // })
 
   
 });
