364a9c7ab6929345ccfd048f063759ab1e3bae8c
Alex Mommersteeg <alexmommersteeg@gmail.com>
2021-07-31 14:20:13 -0400

    Add image viewer

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -10,6 +10,22 @@ $(document).ready(function(){
   $(window).on('resize', function(){
     $("#mobile-nav-id").css('max-height','0');
   });
-
-  
 });
+
+// You should import the CSS file.
+// import 'viewerjs/dist/viewer.css';
+import Viewer from 'viewerjs';
+
+// View an image.
+const viewer = new Viewer(document.getElementById('image'), {
+  inline: true,
+  viewed() {
+    viewer.zoomTo(1);
+  },
+});
+// Then, show the image by clicking it, or call `viewer.show()`.
+
+// View a list of images.
+// Note: All images within the container will be found by calling `element.querySelectorAll('img')`.
+const gallery = new Viewer(document.getElementById('main'));
+// Then, show one image by click it, or call `gallery.show()`.
